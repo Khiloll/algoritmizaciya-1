351d996f5721ab302eec5ba8105656fbcb6b3ecf
Hilol <97358655+Khiloll@users.noreply.github.com>
2023-10-08 22:09:29 +0300

    Add files via upload

---
+++ code/alg 16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int F(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        if (n % 3 == 2 && n > 0)
+        {
+            return F(n - 1) + 1;
+        }
+        if (n % 3 < 2)
+        {
+            return F((n - n % 3) / 3);
+        }
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        int i = 0;
+        while (F(i) != 6)
+        {
+            i++;
+        }
+        Console.WriteLine(i);
+    }
+}
---
+++ code/alg 17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int count = 0;
+        int m = 0;
+        
+        using (StreamReader reader = new StreamReader("17.txt"))
+        {
+            string[] lines = File.ReadAllLines("17.txt");
+            int[] l = Array.ConvertAll(lines, int.Parse);
+
+            for (int i = 0; i < l.Length - 1; i++)
+            {
+                for (int j = i + 1; j < l.Length; j++)
+                {
+                    if ((l[i] + l[j]) % 10 == 0)
+                    {
+                        count++;
+                        m = Math.Max(m, l[i] + l[j]);
+                    }
+                }
+            }
+        }
+        
+        Console.WriteLine(count + " " + m);
+    }
+}
